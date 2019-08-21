db93869fe83f9e589dd710ecaff700f0a80feb64
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-08-21 09:04:55 -0700

    Completed for TL hours

--- React-Router-Movies-master/client/src/Movies/MovieCard.js
+++ React-Router-Movies-master/client/src/Movies/MovieCard.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
 const MovieCard = props => {
-  return;
+  const { title, director, metascore, stars } = props.movie;
+  return (
+    <div className="movie-card">
+      <h2>{title}</h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MovieCard;
+
+// == = value
+// a true statement would be 0 == 0
+// a true statement would be 0 == "0"
+
+// === = value && type
+// a true statement would be 0 === 0
+// a false statement would be 0 === "0"
