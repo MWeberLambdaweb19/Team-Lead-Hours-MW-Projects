8d3565008b99448e1b9f717e060d3c133a535e35
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-08-22 05:48:46 -0700

    Reset movies project

--- React-Router-Movies-master/client/src/index.js
+++ React-Router-Movies-master/client/src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Coffee} from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-<Coffee>
-    <App />
-</Coffee>, document.getElementById('root'));
-
-// I am importing BrowserRouter as Coffee to prove that Router can be anything, it is a banana word!
+ReactDOM.render(<App />, document.getElementById('root'));
